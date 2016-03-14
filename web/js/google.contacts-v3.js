697a9de445c14cda7c184059b542a050aa6e0fe8
androklis <androklis.greg@gmail.com>
2016-03-14 22:31:25 +0200

    Google Contacts Bug Fixed v1.0

--- web/js/google.contacts-v3.js
+++ web/js/google.contacts-v3.js
@@ -10,10 +10,21 @@ function fetchContacts() {
     $.get("https://www.google.com/m8/feeds/contacts/default/full?alt=json&access_token=" + $.cookie("access_token") + "&max-results=100&v=3.0",
             function (response) {
                 $.each(response.feed.entry, function (index, value) {
-                    if (value.link[0].gd$etag) {
-                        $('select#contacts').append($('<option value="' + value.gd$email[0].address + '" data-icon="' + (value.link[0].href).replace('?v=3.0', '').trim() + "?access_token=" + $.cookie("access_token") + '" class="left circle">' + value.title.$t + '</option>'));
-                    } else {
-                        $('select#contacts').append($('<option value="' + value.gd$email[0].address + '" data-icon="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg?sz=50" class="left circle">' + value.title.$t + '</option>'));
+                    if (value.gd$email) {
+                        if (value.title.$t.length > 0) {
+                            if (value.link[0].gd$etag) {
+                                $('select#contacts').append($('<option value="' + value.gd$email[0].address + '" data-icon="' + (value.link[0].href).replace('?v=3.0', '').trim() + "?access_token=" + $.cookie("access_token") + '" class="left circle">' + value.title.$t + '</option>'));
+                            } else {
+                                $('select#contacts').append($('<option value="' + value.gd$email[0].address + '" data-icon="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg?sz=50" class="left circle">' + value.title.$t + '</option>'));
+                            }
+                        } else {
+                            if (value.link[0].gd$etag) {
+                                $('select#contacts').append($('<option value="' + value.gd$email[0].address + '" data-icon="' + (value.link[0].href).replace('?v=3.0', '').trim() + "?access_token=" + $.cookie("access_token") + '" class="left circle">' + value.gd$email[0].address + '</option>'));
+                            } else {
+                                $('select#contacts').append($('<option value="' + value.gd$email[0].address + '" data-icon="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg?sz=50" class="left circle">' + value.gd$email[0].address + '</option>'));
+                            }
+                        }
+
                     }
                 });
                 $('select#contacts').material_select();
